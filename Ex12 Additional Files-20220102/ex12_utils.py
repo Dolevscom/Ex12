b73d51a364037f1bba02b61f049af033189c4d76
dolev <dolev.harel@mail.huji.ac.il>
2022-01-05 16:21:25 +0200

    all test except long_dict

--- Ex12 Additional Files-20220102/ex12_utils.py
+++ Ex12 Additional Files-20220102/ex12_utils.py
@@ -29,7 +29,7 @@ def coord_legal_kernel(coord):
     return final_cor
 
 
-def check_all_path(board, path):
+def check_all_path(board, path, words):
     mx_rows = len(board)
     mx_cols = len(board[0])
     for i in path:
@@ -39,6 +39,9 @@ def check_all_path(board, path):
         if (i_index + 1) <= len(path) - 1:
             if path[i_index + 1] not in coord_legal_kernel(i):
                 return False
+    for word in words:
+        if len(path) > len(word):
+            return False
     return True
 
 
@@ -51,7 +54,7 @@ def is_valid_path(board, path, words):
     :return:str of the legal word, None if its not legal.
     """
     fit_word = []
-    if check_all_path(board, path) is False:
+    if check_all_path(board, path, words) is False:
         return None
     for i in path:
         r = i[0]
@@ -108,17 +111,20 @@ def paths_for_every_coord(n, board, words):
 
 
 def find_length_n_paths(n, board, words):
+    if n == 0:
+        return []
     return paths_for_every_coord(n, board, words)
 
 
-def wrd_vr_al_pos_pths(board, n, words, current_path, path_list, ):
+def word_vr_all_pos(board, n, words, current_path, path_list):
     current_word = ""
-    len_of_word = len(current_word)
+    # len_of_word = len(current_word)
     if not next_possible_move(current_path):
         return
 
     for cord in current_path:
         current_word += board[cord[0]][cord[1]]
+    len_of_word = len(current_word)
 
     for direct in next_possible_move(current_path):
         # current_len = len(current_path)
@@ -136,43 +142,87 @@ def wrd_vr_al_pos_pths(board, n, words, current_path, path_list, ):
             all_possible_paths(board, n, words, current_path + [direct], path_list)
 
 
-def wrd_vr_pths_fr_vry_crd(n, board, words):
+def word_vr_for_every_cord(n, board, words):
     mx_rows = len(board)
     mx_cols = len(board[0])
     path_list = []
     for i in range(mx_rows):
         for j in range(mx_cols):
-            wrd_vr_al_pos_pths(board, n, words, [(i, j)], path_list)
+            word_vr_all_pos(board, n, words, [(i, j)], path_list)
     return path_list
 
 
 def find_length_n_words(n, board, words):
-    return wrd_vr_pths_fr_vry_crd(n, board, words)
+    if n == 0:
+        return []
+    return word_vr_for_every_cord(n, board, words)
 
 
-def max_score_paths(board, words):
-    pass
+# def scr_vr_all_pos(board, n, words, current_path, word_n_path_dict):
+#     current_word = ""
+#     if not next_possible_move(current_path):
+#         return
+#
+#     for cord in current_path:
+#         current_word += board[cord[0]][cord[1]]
+#     len_of_word = len(current_word)
+#
+#     for direct in next_possible_move(current_path):
+#
+#         if len_of_word == n and current_word in words:
+#             word_n_path_dict[current_word] = current_path
+#             # print(f"(FOUND): word: {current_word}, current_path: {current_path}, path_list: {path_list}")
+#             all_possible_paths(board, n, words, current_path + [direct], word_n_path_dict)
+#             return
+#
+#         elif len_of_word > n:
+#             return
+#
+#         elif len_of_word < n:
+#             all_possible_paths(board, n, words, current_path + [direct], word_n_path_dict)
+#
+# def scr_vr_for_every_cord(n, board, words):
+#     mx_rows = len(board)
+#     mx_cols = len(board[0])
+#     word_n_path_dict = {}
+#     for i in range(mx_rows):
+#         for j in range(mx_cols):
+#             word_vr_all_pos(board, n, words, [(i, j)], word_n_path_dict)
+#     return word_n_path_dict
+#
+# def filter_by_score(word_n_path_dict):
+#     filtered_list = []
+#     duplicates = set(x.name for x in d if x.name in seen or seen.add(x.name))
+#     for word in word_n_path_dict:
+#         if word
+#
+# def max_score_paths(board, words):
+#     pass
 
 
-# board = [['a', 'b', 'c', 'd'], ['t', 'r', 'g', 'm'], ['x', 'y', 'z', 'n'], ['h', 'j', 'k', 'l']]
-# path = [(0, 0), (1, 1), (1, 2), (1, 3), (1, 4)]
-# words = {'ab': True, 'cc': True, 'arg': True}
 
-# print( check_all_path(board, path))
-# board הוא רשימה של רשימות של מחרוזות
-    # [['R', 'E', 'J', 'E'], ['H', 'S', 'A', 'U'], ['W', 'O', 'W', 'I'], ['E', 'T', 'A', 'T']]
-    # אבל! לאו דווקא בצורה הבאה.
-    #path הוא רשימה של טאפלס, כאשר כל איבר הוא קוארידנטה וסדר האיברים הוא סדר המילה
-    # אוביקט המכיל את כל המילים החוקיות במשחק מבין האותיות שבלוח
+
 if __name__ == '__main__':
     my_board = [['A', 'B', 'C', 'D'], ['E', 'F', 'G', 'H'], ['I', 'J', 'K', 'L'], ['QU', 'Q', 'U', 'A']]
     needed_path_length = 8
     my_words = ["ABC", "AFKHGJQU", "BAE", "AEI", "AFG"]
     possible_paths = []
+    board1 = [['Q', 'Q', 'Q', 'Q'],
+             ['D', 'O', 'G', 'Q'],
+             ['Q', 'Q', 'Q', 'Q'],
+             ['Q', 'Q', 'Q', 'Q']]
+    word_dict = {'D': True, 'O': True, 'G': True}
+    print(find_length_n_words(1, board1, word_dict))
+        # expected = [[(1, 0)], [(1, 1)], [(1, 2)]]
+        # assert sorted(find_length_n_words(1, board, word_dict)) == \
+        #        sorted(expected)
     # all_possible_paths(my_board, needed_path_length, my_words, [(0, 0)], possible_paths)
     # print(find_length_n_paths(needed_path_length, my_board, my_words))
     # print(find_length_n_words(needed_path_length, my_board, my_words))
-
-board = [['a', 'b', 'c', 'd'], ['qe', 'r', 'g', 'm'], ['x', 'y', 'z', 'n'], ['h', 'j', 'k', 'l']]
-words = {'ab': True, 'cc': True, 'qe': True, 'arzyqe': True}
+# board = [['a', 'b', 'c', 'd'], ['t', 'r', 'g', 'm'], ['x', 'y', 'z', 'n'], ['h', 'j', 'k', 'l']]
+# path = [(0, 0), (1, 1), (1, 2), (1, 3), (1, 4)]
+# words = {'ab': True, 'cc': True, 'arg': True}
+# [['R', 'E', 'J', 'E'], ['H', 'S', 'A', 'U'], ['W', 'O', 'W', 'I'], ['E', 'T', 'A', 'T']]
+# board = [['a', 'b', 'c', 'd'], ['qe', 'r', 'g', 'm'], ['x', 'y', 'z', 'n'], ['h', 'j', 'k', 'l']]
+# words = {'ab': True, 'cc': True, 'qe': True, 'arzyqe': True}
 # print(find_length_n_paths(2, board, words))
